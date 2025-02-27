aa0c811badd31d1ccc2b24630ad110e52427e16e
koshibat <sbtkouki@g.ecc.u-tokyo.ac.jp>
2025-02-27 16:32:41 +0900

    revise App.test.js for linter

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import {render, screen} from '@testing-library/react';
 import App from './App';
 
 // モックを追加
 jest.mock('react-ga4', () => ({
   initialize: jest.fn(),
-  send: jest.fn()
+  send: jest.fn(),
 }));
 
 test('renders JobApplicationTracker', () => {
