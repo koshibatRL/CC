32865cf2044ef8d570e6691d33a9858e736b76f9
koshibat <sbtkouki@g.ecc.u-tokyo.ac.jp>
2025-02-27 23:49:24 +0900

    debug

--- src/App.test.js
+++ src/App.test.js
@@ -2,15 +2,6 @@ import React from 'react';
 import {render, screen} from '@testing-library/react';
 import App from './App';
 
-// Firebase関連のモジュールをモック化
-jest.mock('./firebase', () => ({
-  auth: {
-    // 必要に応じて最小限のモック実装
-    currentUser: null,
-    signInWithEmailAndPassword: jest.fn(),
-  },
-}));
-
 // モックを追加
 jest.mock('react-ga4', () => ({
   initialize: jest.fn(),
